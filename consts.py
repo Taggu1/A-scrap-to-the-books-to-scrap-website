1352efb05d9ecc37734beab7b690dd1305de5f4f
Taggu <81987232+Taggu1@users.noreply.github.com>
2021-11-19 19:59:57 +0200

    Add files via upload

---
+++ consts.py
@@ -0,0 +1,39 @@
+ui_text = {
+    "ui1":"""
+    Welcome To the Scraper !!!
+    """
+    ,"ui2":"""
+    What do you Want:
+    0 - All books
+    1 - Travel , 2 - Mystery , 3 - Historical Fiction , 4 - Sequential Art
+    5 - Classics , 6 - Philosophy , 7 - Romance , 8 - Womens Fiction
+    9 - Fiction , 10 - Childrens , 11 - Religion , 12 - Nonfiction
+    13 - Music , 14 - Default , 16 - Science Fiction , 17 - Sports and Games
+    19 - Fantasy , 20 - New Adult , 21 - Young Adult , 22 - Science
+    23 - Poetry , 24 - Paranormal , 25 - Art , 26 - Psychology
+    27 - Autobiography , 28 - Parenting , 29 - Adult Fiction , 30 - Humor
+    31 - Horror , 32 - History , 33 - Food and Drink , 34 - Christian Fiction
+    35 - Business , 36 - Biography , 37 - Thriller , 38 - Contemporary
+    39 - Spirituality , 40 - Academic , 41 - Self Help , 42 - Historical
+    43 - Christian , 44 - Suspense , 45 - Short Stories , 46 - Novels
+    47 - Health , 48 - Politics , 49 - Cultural , 50 - Erotica
+    51 - Crime (enter the number):  
+    """,
+    "ui3": "done"
+}
+
+geners  = {
+    "1": "travel_2" , "2":"mystery_3" , "3": "historical-fiction_4" , "4": "sequential-art",
+    "5": "classics_6" ,"6": "philosophy_7" ,"7": "romance_8","8":"womens-fiction_9",
+    "9": "fiction_10" , "10": "childrens_11" , "11": "religio_12" ,"12":"nonfiction_13",
+    "13": "music_14" , "14":"default_15" , "16": "science-fiction_16" , "17": "sports-and-games_17",
+    "19": "fantasy_19","20":"new-adult_20" , "21": "young-adult_21" , "22": "science_22",
+    "23": "poetry_23" ," 24":"paranormal_24" , "25": "art_25" , "26": "psychology_26",
+    "27": "autobiography_27" , "28": "parenting_28" , "29": "adult-fiction_29" , "30": "humor_30",
+    "31": "horror_31" , "32": "history_33" , "33": "food-and-drink_33","34": "christian-fiction_34",
+    "35": "business_35" , "36": "biography_36" , "37": "thriller_37" , "38": "contemporary_38",
+    "39": "spirituality_39" , "40": "academic_40" , "41": "self-help_41" , "42": "historical_42",
+    "43": "christian_43" , "44": "suspense_44" , "45": "short-stories_45" , "46": "novels_47",
+    "47": "health_47" , "48": "politics_48" , "49": "cultural_49" , "50": "erotica_50",
+    "51": "crime_51"
+}
---
+++ main.py
@@ -0,0 +1,54 @@
+from consts import *
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def brain(gen_name): 
+    r = requests.get(f"https://books.toscrape.com/catalogue/category/books/{gen_name}/index.html")
+    soup = BeautifulSoup(r.content,"html.parser")
+    pager = soup.find("ul", class_="pager")
+    books = []
+
+    if pager == None:
+        books.append(soup.find_all("article",class_="product_pod"))
+        titles = [element.find("h3").find('a').get("title") for element in books[0]]
+        links = [element.find("a").get("href")[9:] for element in books[0]]
+    else:
+        sw = pager.find("li",class_="current").text.strip()
+        pages_count = int(sw[-1])
+        
+        for page_number in range(1,pages_count + 1):
+            r = requests.get(f"https://books.toscrape.com/catalogue/category/books/{gen_name}/page-{page_number}.html")
+            soup = BeautifulSoup(r.content,"html.parser")
+            books.append(soup.find_all("article",class_="product_pod"))
+        
+
+        page_number = int(input(f"{pages_count} Pages, What Page do you want: "))
+        titles = [element.find("h3").find('a').get("title") for element in books[page_number - 1]]
+        links = [element.find("a").get("href")[9:] for element in books[page_number - 1]]
+
+    for title_n in range(len(titles)):
+        print(f"Book: {title_n} name: {titles[title_n]} ")
+    user_book_choice = int(input("Choose a book number: "))
+    link = f"https://books.toscrape.com/catalogue/{links[user_book_choice]}"
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content,"html.parser")
+    Description = soup.find_all("p")[3].text
+    print(f"""
+    Description: {Description}
+    Link: {link}
+    """)
+
+
+
+def main():
+    print(ui_text["ui1"])
+    user_genere_number = input(ui_text["ui2"])
+    user_genere = geners[user_genere_number]
+    
+    brain(user_genere)
+
+    print(ui_text["ui3"])
+
+main()
