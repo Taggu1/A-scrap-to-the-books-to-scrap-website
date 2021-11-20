dfc22d74dad00a84e9be0cf79d7d1794747d9877
Taggu <81987232+Taggu1@users.noreply.github.com>
2021-11-20 15:02:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from consts import *
 import requests
 from bs4 import BeautifulSoup
 
-
+     
 
 def brain(gen_name): 
     r = requests.get(f"https://books.toscrape.com/catalogue/category/books/{gen_name}/index.html")
@@ -27,24 +27,35 @@ def brain(gen_name):
         page_number = int(input(f"{pages_count} Pages, What Page do you want: "))
         titles = [element.find("h3").find('a').get("title") for element in books[page_number - 1]]
         links = [element.find("a").get("href")[9:] for element in books[page_number - 1]]
+    number_of_books_in_page = len(titles) 
 
-    for title_n in range(len(titles)):
+    if titles == []:
+        print("No books detected")
+        return
+
+    for title_n in range(number_of_books_in_page):
         print(f"Book: {title_n} name: {titles[title_n]} ")
+
     user_book_choice = int(input("Choose a book number: "))
-    link = f"https://books.toscrape.com/catalogue/{links[user_book_choice]}"
-    r = requests.get(link)
-    soup = BeautifulSoup(r.content,"html.parser")
-    Description = soup.find_all("p")[3].text
-    print(f"""
-    Description: {Description}
-    Link: {link}
-    """)
+
+    if user_book_choice <= number_of_books_in_page:
+        link = f"https://books.toscrape.com/catalogue/{links[user_book_choice]}"
+        r = requests.get(link)
+        soup = BeautifulSoup(r.content,"html.parser")
+        Description = soup.find_all("p")[3].text
+        print(f"""
+        Description: {Description}
+        Link: {link}
+        """)
 
 
 
 def main():
     print(ui_text["ui1"])
     user_genere_number = input(ui_text["ui2"])
+    if user_genere_number not in geners.keys():
+        print("Wrong Choice")
+        return
     user_genere = geners[user_genere_number]
     
     brain(user_genere)
